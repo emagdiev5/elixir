760e88959be913980e1933242b7c50648eba4f5a
root <root@localhost.localdomain>
2019-11-09 11:15:07 -0500

    initial commit

--- lib/kvstore/router.ex
+++ lib/kvstore/router.ex
@@ -1,1 +1,32 @@
-Для веб сервера нужен маршрутизатор, место ему именно тут.
+defmodule KVstore.Router do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  @key   "key"
+  @value "value"
+  @ttl   "ttl"
+
+  get "/create" do
+    conn = fetch_query_params(conn, []) 
+    %{ @key => key, @value => value, @ttl => ttl} = conn.params
+    KVstore.Storage.create({key, value}, ttl)
+    send_resp(conn, 200, "New record with {'#{key}', '#{value}'} created for #{ttl} seconds")
+  end
+
+  get "/read" do
+    conn = fetch_query_params(conn, []) 
+    %{ @key => key } = conn.params
+    value = KVstore.Storage.read(key)
+    send_resp(conn, 200, "#{inspect value}")
+  end
+
+  get "/delete" do
+    conn = fetch_query_params(conn, []) 
+    %{ @key => key } = conn.params
+    send_resp(conn, 200, "record key '#{key}' deleted")
+  end
+
+  match(_, do: send_resp(conn, 404, "Oops!"))
+end
--- lib/kvstore/storage.ex
+++ lib/kvstore/storage.ex
@@ -1,1 +1,24 @@
-Этот модуль должен реализовать механизмы CRUD для хранения данных. Если одного модуля будет мало, то допускается создание модулей с префиксом "Storage" в названии.
+defmodule KVstore.Storage do
+    
+    def create(obj, ttl) do
+        :dets.open_file(:disc_storage, [type: :set])
+        :dets.insert_new(:disc_storage, obj)
+        :timer.apply_after(String.to_integer(ttl)*1000, :dets, :delete, :disc_storage)
+    end
+
+    def read(obj) do
+        try do
+            :dets.lookup(:disc_storage, obj)
+        rescue
+            ArgumentError -> "record expired"
+        end    
+    end
+    
+    def update(obj) do
+        :dets.insert(:disc_storage, obj)
+    end
+
+    def delete(obj) do
+        :dets.delete(:disc_storage, obj)
+    end
+end
