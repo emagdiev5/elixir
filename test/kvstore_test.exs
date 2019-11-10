757c8a7dfa4cbcff29fbd4cbae13fcb4082d0075
root <root@localhost.localdomain>
2019-11-09 22:59:34 -0500

    unit tests added

--- test/kvstore_test.exs
+++ test/kvstore_test.exs
@@ -1,1 +1,18 @@
-Тестируем как можно больше кейсов.
+defmodule KVstoreTest do
+    use ExUnit.Case
+
+    test "can create record" do
+        url = String.to_charlist("http://localhost:888/create?key=newkey&value=newvalue&ttl=10")
+        :httpc.request(url)
+        record = :dets.lookup(:disc_storage, "newkey")
+        assert record != nil, "record is not created"
+    end
+
+    test "record deleted after 10 seconds" do
+        url = String.to_charlist("http://localhost:888/create?key=newkey&value=newvalue&ttl=10")
+        :httpc.request(url)
+        record = :dets.lookup(:disc_storage, "newkey")
+        :timer.sleep(11000)
+        assert record != nil, "11 seconds passed, record not deleted"
+    end
+end
